94eb7e1618cf3ae83a4c7e0ca0aa4ab8657979c9
shengxiadiaoling <zhiyu3@qq.com>
2019-08-12 16:59:50 +0800

    Initial commit

---
+++ src/main/java/com/zhiyu/pm_modbus/PmModbusApplication.java
@@ -0,0 +1,13 @@
+package com.zhiyu.pm_modbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmModbusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmModbusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhiyu/pm_modbus/PmModbusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhiyu.pm_modbus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PmModbusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
